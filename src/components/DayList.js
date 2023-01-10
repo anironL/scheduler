40268e3fcf6845ac926d2ee5d1f531a51aaa6088
anironL <viasolaris@gmail.com>
2023-01-10 13:59:33 -0500

    Update DayList
    Add function to daylist to populate instead of hardcoding values

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,30 +5,21 @@ import React from "react";
 import DayListItem from "./DayListItem"
 
 export default function DayList(props){
+  const days = props.days.map(day => {
+    return (
+      <DayListItem 
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={day.setDay}  
+      />
+    )
+  });
 
   return(
     <ul>
-      <DayListItem 
-        key={props.days[0].id}
-        name={props.days[0].name} 
-        spots={props.days[0].spots} 
-        selected={props.days[0].name === props.day}
-        setDay={props.setDay}  
-      />
-      <DayListItem
-        key={props.days[1].id} 
-        name={props.days[1].name} 
-        spots={props.days[1].spots} 
-        selected={props.days[1].name === props.day}
-        setDay={props.setDay}  
-      />
-      <DayListItem 
-        key={props.days[2].id}
-        name={props.days[2].name}
-        spots={props.days[2].spots} 
-        selected={props.days[2].name === props.day}
-        setDay={props.setDay}  
-      />      
+      {days}
     </ul>
   )
 }
