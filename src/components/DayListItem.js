af46ca9d8a83d1883049a86125044c8514e100ca
anironL <viasolaris@gmail.com>
2023-01-10 13:19:49 -0500

    Test DayListItem FormatSpots function
    Move spots remaining component rendering logic to a function FormatSpots and test (all passing)

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,16 @@ import classNames from "classnames";
 
 import "./DayListItem.scss";
 
+const FormatSpots = (props) => {
+  return (
+    <>
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+    </>
+  )
+}
+
 export default function DayListItem(props) {
   //  day-list__item all the time
   //  day-list__item--selected class name if props.selected is true
@@ -13,13 +23,11 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   })
-  
-    return (
+
+  return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
-      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
-      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      <FormatSpots spots={props.spots} />
     </li>
   );
 }
