15cb0a09e8dfeb0a2a5d991327a5249d78c91a2f
anironL <viasolaris@gmail.com>
2023-01-11 11:03:28 -0500

    Import styles for Appointment components

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./styles.scss";
+
 export default function Empty (props) {
   return (
     <main className="appointment__add">
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Application () {
   return (
