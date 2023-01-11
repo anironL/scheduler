fa1c99bc27725bbceeede35faa9c397fcb550bfa
anironL <viasolaris@gmail.com>
2023-01-11 11:06:17 -0500

    Update Show.js props
    Show.js takes props (placeholders/expected) from storybook

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -8,7 +8,7 @@ export default function Show (props) {
       <section className="appointment__card-left">
         <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
-          <h4 className="text--light">{props.interviewer.name}</h4>
+          <h4 className="text--light">Interviewer</h4>
           <h3 className="text--regular">{props.interviewer.name}</h3>
         </section>
       </section>
@@ -18,13 +18,13 @@ export default function Show (props) {
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
-            onEdit={props.onEdit} 
+            onClick={props.onEdit}
           />
           <img
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
-            onDelete={props.onDelete}
+            onClick={props.onDelete}
           />
         </section>
       </section>
