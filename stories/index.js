83dee242890ea92bbfa88cb408c69804c39e427c
anironL <viasolaris@gmail.com>
2023-01-11 11:15:02 -0500

    Add confirm.js & storybook
    Add confirm component to Appointment & storybook init

--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
-
+import Confirm from "components/Appointment/Confirm.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -159,6 +159,15 @@ storiesOf("Appointment", module)
       onEdit={action("onEdit")}
       onDelete={action("onDelete")} 
     />
-  );
+  )
+  .add("Confirm", () => 
+    <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")} 
+    />
+  )
+
+
 
 
