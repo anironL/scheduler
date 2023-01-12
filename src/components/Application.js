7c23a03873c5e01877d5600c883bcf74ba8fe635
anironL <viasolaris@gmail.com>
2023-01-12 18:07:03 -0500

    Retrieve appointments from api & add promise.all
    Remove depreciated and commented out code

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,73 +3,40 @@ import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index.js";
+import { getAppointmentsForDay } from "components/helpers/selectors.js";
 
 import "components/Application.scss";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-const renderAppointments = Object.values(appointments).map(appointment => {
-  return (
-    <Appointment 
-      key={appointment.id} 
-      {...appointment} 
-    />
-  )
-})
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    // appointments: {}
+    appointments: {}
   });
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const renderAppointments = dailyAppointments.map(appointment => {
+    return (
+      <Appointment 
+        key={appointment.id} 
+        {...appointment} 
+      />
+    )
+  })
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get("/api/days")
-    .then (response => {
-      setDays(response.data)
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      // axios.get("/api/interviewers")
+    ])
+    .then((all) => {
+      console.log("promise days:", all[0].data);
+      console.log("promise appointments:", all[1].data);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     })
-    .catch (err => {console.log(err)})
   }, [])
 
   return (
