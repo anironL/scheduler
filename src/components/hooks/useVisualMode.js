d14e42452c1856c7c22d098fe08ce3410443dbf7
anironL <viasolaris@gmail.com>
2023-01-13 14:43:36 -0500

    Add custom hook for appointment transitions
    Add custom hook for transitions

---
+++ src/components/hooks/useVisualMode.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+// function useCustomHook() {
+//   function action() {}
+
+//   return { action };
+// }
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  // act(() => result.current.transition(SECOND));
+  const transition = (newMode, replace = false) => {
+    if (replace === true) {
+      history[history.length-1] = newMode
+      setHistory(history)
+    } 
+    if (replace === false) {
+      setHistory([...history, newMode])
+    }
+    setMode(newMode)
+  };
+
+  const back = () => {
+    if (history.length < 3) {
+      setMode(history[0])
+    } else {
+      setMode(history[history.length - 2])
+      setHistory(prevHistory => { 
+        prevHistory.pop()
+        return prevHistory    
+      })
+    } 
+  }
+
+  return { mode, transition, back };
+};
