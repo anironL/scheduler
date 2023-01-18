1c1232f2120e37561a8dbbc0af4d43bdd0974199
anironL <viasolaris@gmail.com>
2023-01-18 13:37:24 -0500

    Complete E2E Cypress testing

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -1,13 +1,15 @@
-// Return array of appointmentIds for the provided day. 
+// Return array of appointmentIds for the provided day.
 export function getAppointmentsForDay(state, day) {
   if (state.days.length === 0) {
     return [];
   }
-  const findDay = state.days.find(days => days.name === day)
+  const findDay = state.days.find((days) => days.name === day);
   if (!findDay) {
     return [];
   }
-  return findDay.appointments.map(appointmentId => state.appointments[appointmentId])
+  return findDay.appointments.map(
+    (appointmentId) => state.appointments[appointmentId]
+  );
 }
 
 // Return interview object for the provided interviewId
@@ -15,22 +17,24 @@ export function getInterview(state, interview) {
   if (interview === null) {
     return null;
   }
- 
+
   return {
-    student:interview.student,
-    interviewer:state.interviewers[interview.interviewer],
-  }
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer],
+  };
 }
 
-// Return array of interviewerIds for the provided day. 
+// Return array of interviewerIds for the provided day.
 export function getInterviewersForDay(state, day) {
   if (day === null) {
     return [];
   }
-  const findDay = state.days.find(days => days.name === day)
+  const findDay = state.days.find((days) => days.name === day);
   if (!findDay) {
     return [];
   }
- 
-  return findDay.interviewers.map(interviewerId => state.interviewers[interviewerId])
+
+  return findDay.interviewers.map(
+    (interviewerId) => state.interviewers[interviewerId]
+  );
 }
